f12312e7691b923b2af4a3c97597fa5a48581cb8
Ouhemna Mohamed Anes <om_ouhemna@esi.dz>
2026-07-17 21:16:02 +0100

    just committ

--- ReAct/react_agent.py
+++ ReAct/react_agent.py
@@ -1,16 +1,16 @@
 from typing import Annotated , TypedDict , Sequence
 from dotenv import load_dotenv
 from langgraph.graph import StateGraph ,START ,END
-from langchain_core.messages import BaseMessage
-from langchain_core.messages import ToolMessage
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import BaseMessage # all messages inherites from this  
+from langchain_core.messages import ToolMessage # the result of tool wrapped inside his toolmessage after that returned to llm
+from langchain_core.messages import SystemMessage #this tell the llm how o behave ex : you are helpful assistant
 from langchain_groq import ChatGroq
-from langchain_core.tools import tool
-from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool #tools are python functions and they named tools becaus the llm can call them and use them 
+from langgraph.graph.message import add_messages #is a reducer instead of replacing old messages it appends
+from langgraph.prebuilt import ToolNode #without this we should execute tools by oourselfs so this execute tools
 
 load_dotenv()
 
 class AgentState(TypedDict):
     messages : Annotated[Sequence[BaseMessage] , add_messages]
-    
+
