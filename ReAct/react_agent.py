965b807e3ca902116646f935ec701e8a2328124d
Ouhemna Mohamed Anes <om_ouhemna@esi.dz>
2026-07-18 16:07:00 +0100

    new react agent

--- ReAct/react_agent.py
+++ ReAct/react_agent.py
@@ -14,3 +14,66 @@ load_dotenv()
 class AgentState(TypedDict):
     messages : Annotated[Sequence[BaseMessage] , add_messages]
 
+@tool
+def add(a:int , b :int ):
+    """this is an addition fnction that adds 2 numbers together  """
+    return a+b 
+
+@tool 
+def subtract(a:int , b :int)  :
+    """subtraction function"""
+    return a - b 
+
+@tool 
+def multiply(a:int , b :int)  :
+    """mutlitplication function"""
+    return a*b 
+
+tools = [add , subtract , multiply]
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+def llm_call(state:AgentState) ->AgentState:
+    system_prompt= SystemMessage(content="" \
+    "You are my AI assistant , please answer my query to the best of your ability . ")
+    response = llm.invoke([system_prompt] + state["messages"])
+    return{"messages" : [response]}
+def should_continue(state : AgentState):
+    messages = state["messages"]
+    last_meassge = messages[-1]
+    if not last_meassge.tool_calls:
+        return "end"
+    else :
+        return "continue"
+    
+graph = StateGraph(AgentState)
+graph.add_node("our_agent" , llm_call)
+tool_node = ToolNode(tools = tools)
+graph.add_node("tools", tool_node)
+
+graph.add_edge(START , "our_agent")
+
+graph.add_conditional_edges(
+    "our_agent" , 
+    should_continue,
+    {
+        "continue" : "tools" , 
+        "end" : END
+
+    },
+)
+graph.add_edge("tools" , "our_agent")
+
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {"messages": [("user", "Add 40 + 12 and then multiply the result by 6. Also tell me a joke please.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
+
